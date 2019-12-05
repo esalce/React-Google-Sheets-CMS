f84503f6a4ad18d14d459443df851f25fe4165c8
Eddie Salce <salce048@gmail.com>
2019-12-04 22:20:18 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Tabletop from 'tabletop';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    Tabletop.init({
+      key: '1pHzuMLRVWDAjSXhcWvIwA1-cpZI97V9Qh6m3jkXXCQg',
+      callback: googleData => {
+        this.setState({
+          data: googleData
+        })
+      },
+      simpleSheet: true
+    })
+  }
+
+  render() {
+    const { data } = this.state
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">React + Google Sheets Demo</h1>
+        </header>
+        <div id="employee-details">
+          {
+            data.map(obj => {
+              return (
+                <div key={obj.employee}>
+                  <p>{obj.employee}</p>
+                  <p>{obj.favDog}</p>
+                  <img alt={obj.favDog} src={obj.img} />
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
